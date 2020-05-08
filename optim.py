b6e1faa165353fd5f21a368bcd5bbd6683698de9
salarim <salari.m1375@gmail.com>
2020-05-08 14:58:13 -0700

    Add ContrastiveLoss

--- optim.py
+++ optim.py
@@ -39,3 +39,92 @@ def triplet_loss(anchor_emb, pos_emb, neg_emb, target, margin=0.02):
     # loss_embedd = anchor_emb.norm(2) + pos_emb.norm(2) + neg_emb.norm(2)
     loss = loss_triplet #+ 0.001 * loss_embedd
     return loss
+
+
+class ContrastiveLoss(torch.nn.Module):
+
+    def __init__(self, device, batch_size_l, batch_size_u, temperature):
+        super(ContrastiveLoss, self).__init__()
+        self.device = device
+        self.batch_size_l = batch_size_l
+        self.batch_size_u = batch_size_u
+        self.batch_size = batch_size_l + batch_size_u
+        self.temperature = temperature
+
+        self._cosine_similarity = torch.nn.CosineSimilarity(dim=-1)
+
+        self.self_sup_negative_mask = self._get_self_sup_negative_mask()
+        self.self_sup_criterion = torch.nn.CrossEntropyLoss(reduction="sum")
+
+    def _get_self_sup_negative_mask(self):
+        diag = np.eye(2 * self.batch_size)
+        l1 = np.eye(2 * self.batch_size,  k=-self.batch_size)
+        l2 = np.eye(2 * self.batch_size, k=self.batch_size)
+        mask = torch.from_numpy((diag + l1 + l2))
+        mask = (1 - mask).type(torch.bool)
+        return mask.to(self.device)
+
+        # l means labeled, u means unlabeled
+    def forward(self, zis_l, zjs_l, zis_u, zjs_u, targets_l):
+        self_sup_representations = torch.cat([zis_l,zis_u,zjs_l,zjs_u], dim=0)
+        self_sup_similarity_matrix = self.cosine_similarity(representations, representations)
+
+        self_sup_loss = self._self_sup_loss(self_sup_similarity_matrix)
+
+        sup_similarity_matrix = self_sup_similarity_matrix[:self.batch_size_l, :self.batch_size_l]
+        sup_loss = _sup_loss(sup_similarity_matrix, targets_l)
+
+        return self_sup_loss + sup_loss
+
+    def cosine_similarity(self, x, y):
+        v = self._cosine_similarity(x.unsqueeze(1), y.unsqueeze(0))
+        return v
+
+    def _self_sup_loss(self, similarity_matrix):
+        l_pos = torch.diag(similarity_matrix, self.batch_size)
+        r_pos = torch.diag(similarity_matrix, -self.batch_size)
+        positives = torch.cat([l_pos, r_pos]).view(2 * self.batch_size, 1)
+
+        negatives = similarity_matrix[self.self_sup_negative_mask].view(2 * self.batch_size, -1)
+
+        logits = torch.cat((positives, negatives), dim=1)
+        logits /= self.temperature
+
+        labels = torch.zeros(2 * self.batch_size).to(self.device).long()
+        loss = self.self_sup_criterion(logits, labels)
+
+        return loss
+
+    def _sup_loss(self, similarity_matrix, targets):
+        logits = similarity_matrix / self.temperature
+        logits = self._drop_diagonal(logits)
+
+        pos_targets = self._get_sup_pos_targets(targets)
+        cross_ent = self.cross_entropy(logits, pos_targets)
+
+        loss = cross_ent.sum()
+
+        return loss
+
+    def _get_sup_pos_targets(self, targets):
+        targets_mat = targets.repeat(targets.shape[0], 1)
+        tmp = targets.unsqueeze(dim=1).repeat(1, targets.shape[0])
+
+        pos_targets = targets_mat == tmp & ~torch.eye(targets.shape[0]).type(torch.bool)
+        pos_targets = pos_targets.type(torch.float32)
+
+        pos_targets = self._drop_diagonal(pos_targets)
+
+        return pos_targets.to(self.device)
+
+    def _drop_diagonal(self, x):
+        mask = ~torch.eye(x.shape[0]).type(torch.bool).to(self.device)
+        return x[mask].view(x.shape[0], -1)
+
+    def cross_entropy(self, output, target):
+        m = torch.nn.LogSoftmax(dim=1)
+        logsoftmax = -m(output)
+        logsoftmax[~target.type(torch.bool)] = 0.0
+        logsoftmax = logsoftmax.sum(dim=1)
+        weights = target.sum(dim=1)
+        return logsoftmax / weights
