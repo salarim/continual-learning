83ec5b74250b28788f233beda383f8f10ef01e4c
Mohammad Hadi Salari <msalari@cedar5.cedar.computecanada.ca>
2020-05-16 13:13:15 -0700

    Update job script

--- job.sh
+++ job.sh
@@ -2,14 +2,14 @@
 #SBATCH --mail-user=salari.m1375@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-mori_gpu
-#SBATCH --job-name=CL
+#SBATCH --job-name=CL-cifar100-parallel
 #SBATCH --output=%x-%j.out
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time=01:00:00
+#SBATCH --time=04:00:00
 #SBATCH --mem=0
 #SBATCH --gres=gpu:p100l:4
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=12
 
 cd $SLURM_TMPDIR
 cp -r ~/scratch/continual-learning .
@@ -22,8 +22,7 @@ pip install --no-index --upgrade pip
 pip install --no-index -r requirements.txt
 
 
-python main.py --batch-size 128 --epochs 2 --model-type softmax \
---tasks 2 --dataset mnist --exemplar-size 1000 --oversample --log-interval 100 \
---multiprocessing-distributed --world-size 1 --rank 0
+python main.py --batch-size 128 --epochs 200 --model-type softmax \
+--tasks 1 --dataset cifar100 --log-interval 100 --test-interval 5
 
 cp experiments/* ~/scratch/continual-learning/experiments
