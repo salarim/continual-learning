0fee9d5bef7592ed84dcb374da9836b5f12bf18f
Mohammad Hadi Salari <msalari@cedar1.cedar.computecanada.ca>
2020-05-22 12:00:27 -0700

    Update job script for training on Imagenet

--- job.sh
+++ job.sh
@@ -2,11 +2,11 @@
 #SBATCH --mail-user=salari.m1375@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-mori_gpu
-#SBATCH --job-name=CL-cifar100-parallel
+#SBATCH --job-name=CL-imagenet
 #SBATCH --output=%x-%j.out
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time=04:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mem=0
 #SBATCH --gres=gpu:p100l:4
 #SBATCH --cpus-per-task=12
@@ -15,14 +15,22 @@ cd $SLURM_TMPDIR
 cp -r ~/scratch/continual-learning .
 cd continual-learning
 
-module load python/3.7
+cp ~/scratch/imagenet/imagenet_object_localization_patched2019.tar .
+echo 'Copied'
+tar -xf imagenet_object_localization_patched2019.tar ILSVRC/Data/CLS-LOC --checkpoint=1000000
+echo 'Extracted!'
+
+module load python/3.7 cuda/10.0
 virtualenv --no-download venv
 source venv/bin/activate
 pip install --no-index --upgrade pip
 pip install --no-index -r requirements.txt
 
 
-python main.py --batch-size 128 --epochs 200 --model-type softmax \
---tasks 1 --dataset cifar100 --log-interval 100 --test-interval 5
+python main.py --batch-size 256 --epochs 200 --model-type softmax \
+--tasks 1 --dataset imagenet --unlabeled-dataset imagenet --data-path ILSVRC/Data/CLS-LOC \
+--log-interval 100 --test-interval 5 --gpu 0
 
 cp experiments/* ~/scratch/continual-learning/experiments
+cp *.pt ~/scratch/continual-learning
+
