c25dc44128cc274a662336a9d2b4e556f3e06e8b
msalari <msalari@sfu.ca>
2020-05-26 19:10:43 -0700

    Add new transforms for contrastive dataset

--- data_utils.py
+++ data_utils.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 import math
 import h5py
 import numpy as np
@@ -78,6 +79,13 @@ class DataLoaderConstructor:
             'imagenet':(0.229, 0.224, 0.225)
         }
 
+        input_shape = {
+            'mnist':(28, 28, 1),
+            'cifar10':(32, 32, 3),
+            'cifar100':(32, 32, 3),
+            'imagenet':(224, 224, 4)
+        }
+
         transforms = []
         if self.config.train:
             if dataset_name in ['cifar10', 'cifar100']:
@@ -90,6 +98,14 @@ class DataLoaderConstructor:
             if dataset_name == 'imagenet':
                 transforms.extend([torchvision.transforms.Resize(256),
                                    torchvision.transforms.CenterCrop(224)])
+
+        if self.config.dataset_type == 'contrastive':
+            color_jitter = torchvision.transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
+
+            transforms.extend([torchvision.transforms.RandomApply([color_jitter], p=0.8),
+                               torchvision.transforms.RandomGrayscale(p=0.2),
+                               GaussianBlur(kernel_size=int(0.1 * input_shape[dataset_name][0]))
+                               ])
         
         transforms.extend([torchvision.transforms.ToTensor(),
                             torchvision.transforms.Normalize(means[dataset_name],
@@ -232,6 +248,27 @@ class ContinualIndexConstructor:
         
         return indexes
 
+
+
+class GaussianBlur(object):
+    # Implements Gaussian blur as described in the SimCLR paper
+    def __init__(self, kernel_size, min=0.1, max=2.0):
+        self.min = min
+        self.max = max
+        # kernel size is set to be 10% of the image height/width
+        self.kernel_size = kernel_size
+
+    def __call__(self, sample):
+        sample = np.array(sample)
+
+        # blur the image with a 50% chance
+        prob = np.random.random_sample()
+
+        if prob < 0.5:
+            sigma = (self.max - self.min) * np.random.random_sample() + self.min
+            sample = cv2.GaussianBlur(sample, (self.kernel_size, self.kernel_size), sigma)
+
+        return sample
 
 
 class SimpleDataset(torch.utils.data.Dataset):
