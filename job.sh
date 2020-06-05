9cc2ef74bf4ec4cb3a47b0747cab105dc9369f1f
Mohammad Hadi Salari <msalari@cedar5.cedar.computecanada.ca>
2020-06-05 13:06:56 -0700

    Train contrastive in job script

--- job.sh
+++ job.sh
@@ -2,13 +2,13 @@
 #SBATCH --mail-user=salari.m1375@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-mori_gpu
-#SBATCH --job-name=CL-cifar100
+#SBATCH --job-name=contrastive-1task-self-sup
 #SBATCH --output=%x-%j.out
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time=03:00:00
+#SBATCH --time=05:00:00
 #SBATCH --mem=0
-#SBATCH --gres=gpu:p100:1
+#SBATCH --gres=gpu:p100l:4
 #SBATCH --cpus-per-task=12
 
 cd $SLURM_TMPDIR
@@ -27,9 +27,14 @@ pip install --no-index --upgrade pip
 pip install --no-index -r requirements.txt
 
 
-python main.py --batch-size 128 --epochs 200 --model-type softmax \
---tasks 1 --dataset cifar100 --unlabeled-dataset cifar100 \
---log-interval 100 --test-interval 5 --gpu 0
+# sup_coef=`echo "$SLURM_ARRAY_TASK_ID/20" | bc -l`
+
+python main.py --lr 3e-4 --batch-size 512 --weight-decay 1e-5 --epochs 80 \
+--model-type contrastive --tasks 1 --dataset cifar10 --unlabeled-dataset cifar10 \
+--log-interval 100 --test-interval 5 --milestones 5 --gamma 0.1 \
+--sup-coef 0.0 --save-model
 
 cp experiments/* ~/scratch/continual-learning/experiments
+cp *.pt ~/scratch/continual-learning/
+cp -r plots ~/scratch/continual-learning/
 
