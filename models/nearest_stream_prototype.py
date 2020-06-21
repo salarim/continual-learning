Remove exponential average from NearestStreamPrototype, Bug fix in train_contrastive

models/nearest_stream_prototype.py
@@ -3,18 +3,14 @@ import torch
 
 class NearestStreamPrototype:
 
-    def __init__(self, sigma, beta=0.997):
+    def __init__(self, sigma):
         self.sigma = sigma
-        self.beta = beta
 
         self.class_prototypes = {}
-        self.batch_num = 0
 
     def add_features(self, prev_feats, cur_feats, targets):
-        self.batch_num += 1
-
         if prev_feats is not None:
-            self._update_prototypes(prev_feats, cur_feats)
+            self._update_prototypes(prev_feats, cur_feats, targets)
 
         self._add_cur_batch_prototypes(cur_feats, targets)
 
@@ -36,19 +32,20 @@ class NearestStreamPrototype:
         return class_targets[nearest_idxs]
         
 
-    def _update_prototypes(self, prev_feats, cur_feats):
-        absent_targets = [target for target, (_, batch_num) in self.class_prototypes.items() \
-                          if batch_num < self.batch_num]
+    def _update_prototypes(self, prev_feats, cur_feats, targets):
+        unique_targets = torch.unique(targets)
+        absent_targets = [target for target in self.class_prototypes.keys() \
+                          if target not in unique_targets]
 
         for target in absent_targets:
-            target_prev_proto, target_prev_batch_num = self.class_prototypes[target]
+            target_prev_proto, target_prev_num = self.class_prototypes[target]
 
             weights = (((prev_feats - target_prev_proto)**2).sum(dim=1).sqrt() \
                 / (-2*self.sigma)).exp()
             drifts = cur_feats - prev_feats
             target_estimated_drift = (drifts * weights.unsqueeze(1)).sum(dim=0) / (weights.sum() + 1e-8)
             target_estimated_proto = target_prev_proto + target_estimated_drift
-            self.class_prototypes[target] = (target_estimated_proto, target_prev_batch_num)
+            self.class_prototypes[target] = (target_estimated_proto, target_prev_num)
 
 
     def _add_cur_batch_prototypes(self, feats, targets):
@@ -56,10 +53,10 @@ class NearestStreamPrototype:
             target_feats = feats[targets == target]
             target = target.item()
             if target not in self.class_prototypes:
-                self.class_prototypes[target] = (target_feats.mean(dim=0), self.batch_num)
+                self.class_prototypes[target] = (target_feats.mean(dim=0), target_feats.shape[0])
             else:
-                proto, target_last_batch_num = self.class_prototypes[target]
-                new_proto = target_feats.mean(dim=0)
-                beta_i = self.beta ** (self.batch_num - target_last_batch_num)
-                new_proto = beta_i * proto + (1 - beta_i) * new_proto
-                self.class_prototypes[target] = (new_proto, self.batch_num)
+                sum_feats = target_feats.sum(dim=0) + \
+                            self.class_prototypes[target][0] * \
+                            self.class_prototypes[target][1]
+                num = target_feats.shape[0] + self.class_prototypes[target][1]
+                self.class_prototypes[target] = (sum_feats / num, num)